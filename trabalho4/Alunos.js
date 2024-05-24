be70427df15f96616c2faea510b67745423f63bb
Maria Paula Bastos <mapabastos@gmail.com>
2024-05-24 15:41:03 -0300

    anotações e comentarios

--- trabalho4/Alunos.js
+++ trabalho4/Alunos.js
@@ -1,5 +1,6 @@
-// Classe para armazenar os dados de todos os alunos
+// classe para armazenar os dados de todos os aluninhs
 class Alunos {
+  //campos pertencentes ao aluno
   campos_aluno = [
     "cod_curso",
     "conceito",
@@ -12,63 +13,73 @@ class Alunos {
     "nome_curso"
   ];
 
+  // consrutor que inicializa a estrutura para armaznar os alunos
   constructor() {
     this.alunos = {};
   }
 
+  // Método para inserir dados de um aluno
   insere(dados) {
     var matr_aluno = dados["matr_aluno"];
-
     var materia = {};
     var aluno = {};
-    // cria a entrada da materia com os campos de matéria
-    // (ou seja, que não pertencem ao aluno)
+
+    // separa os dados da matéria (campos que não pertencem ao alunso)
     for (let key of Object.keys(dados)) {
       if (!(this.campos_aluno.includes(key))) {
         materia[key] = dados[key];
       }
     }
-    // cria entrada do aluno se não existe
+
+    // noa entrada : se ele ainda não estiver cadastrado
     if (!(matr_aluno in this.alunos)) {
       for (let campo of this.campos_aluno) {
         aluno[campo] = dados[campo];
+        console.log("aluno - 2")
       }
-      aluno["materias"] = new Materias();
-      // e o inclui nos alunos cadastrados
-      this.alunos[matr_aluno] = aluno;
+      aluno["materias"] = new Materias(); // Inicializa as matérias do aluno
+      this.alunos[matr_aluno] = aluno; // Adiciona o aluno ao registro de alunos
     }
-    // adiciona a matéria do aluno
+
+    // Adiciona a matéria ao registro de matérias do aluno
     this.alunos[matr_aluno]["materias"].insere(materia);
+    console.log("aluno - 1")
   }
- 
-  // "Apaga" os alunos salvos
+
+  // limpa limpa limpa
   limpa() {
     this.alunos = {};
   }
-  // Retorna os GRRs de todos os alunos solvos
+
+  // Mobter todos os GRRs salvicados
   getGRRs() {
+    console.log("aluno - 3 - chamou")
     return Object.keys(this.alunos);
+    
   }
-  // Ordena as matérias de todos os alunos salvos de acordo com semestre/ano cursado
+
+  // ordenar as matérias dos alunos de acordo com semestre/ano cursado
   ordena() {
+    console.log("aluno - AAAAAAAAAAAAA");
     const listaGRR = this.getGRRs();
     for (let grr of listaGRR) {
       this.alunos[grr]["materias"].ordena();
     }
   }
-  // Retorna os dados de um aluno com base em seu GRR
+
+  // otber  dados de um aluno com GRR
   getAluno(GRR) {
     return this.alunos[GRR];
   }
 
-
-  // Método para acessar o conceito de um aluno específico
+  //  acessar o conceito de um aluno específico
   getConceito(GRR) {
     const aluno = this.alunos[GRR];
     if (aluno) {
+      console.log("aluno - 77554", aluno.conceito);
       return aluno.conceito;
     } else {
-      return null; // Ou qualquer valor de retorno que você preferir para indicar que o aluno não foi encontrado
+      return null; // R null se  não for encontrado
     }
   }
 }
